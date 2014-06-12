80246c4fd1186f3e42dfcc5575d7416683b1c299
Rudger Gravestein <rudgergravestein@gmail.com>
2014-06-13 00:40:18 +0200

    Add cookbook param to virtualhost

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.3.2'
+version          '0.3.3'
 
 depends 'baseserver', '~> 0.7.3'
 
