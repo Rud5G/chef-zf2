c9276870d1cdb2cbc4cd08feaf9743309f4f263a
Rudger Gravestein <rudgergravestein@gmail.com>
2015-02-07 15:28:50 +0100

    Bump version: 0.6.0
    Add config option: bool support_windows_clients

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.5.5'
+version          '0.6.0'
 
 depends 'baseserver', '~> 0.7.10'
 
