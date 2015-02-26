0e4e64aea47adaa3c3104531ca2164ce0c584a9a
Rudger Gravestein <rudgergravestein@gmail.com>
2015-02-26 08:17:51 +0100

    Move nodejs, npm, grunt, bower to recipe
    Fixes #34

--- recipes/development.rb
+++ recipes/development.rb
@@ -54,16 +54,6 @@ if node.chef_environment == 'development'
   # samba client
   include_recipe 'samba::default'
 
-
-  # nodejs for grunt
-  include_recipe 'nodejs'
-
-  bash 'install-grunt-simple' do
-    code <<-EOH
-      npm install -g grunt-cli
-    EOH
-  end
-
   # mailcatcher
   include_recipe 'zf2::mailcatcher'
 
