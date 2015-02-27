79512e5a4f5f764ad1cbc6fd634ceb2391ba3025
Rudger Gravestein <rudgergravestein@gmail.com>
2015-02-27 16:18:52 +0100

    Set all version, add mysql2-gem

--- metadata.rb
+++ metadata.rb
@@ -4,16 +4,19 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.6.3'
+version          '0.7.0'
+
 
 depends 'baseserver', '~> 0.7.10'
 
-depends 'php', '~> 1.5.0'
-depends 'database', '~> 4.0.2'
-depends 'mysql', '~> 6.0.13'
 depends 'apache2', '~> 3.0.1'
+depends 'php', '~> 1.5.0'
 
-depends 'nodejs'
+depends 'database', '~> 4.0.3'
+depends 'mysql', '~> 6.0.15'
+depends 'mysql2_chef_gem', '~> 1.0.1'
+
+depends 'nodejs', '~> 2.2.0'
 
 # development
 depends 'samba', '~> 0.12.0'
