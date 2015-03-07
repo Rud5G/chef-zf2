15b39aca1447b087d723e01e9accab76bcdfd4a3
Rudger Gravestein <rudgergravestein@gmail.com>
2015-03-07 15:45:50 +0100

    Add explicit logrotate, not just through baseserver

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,15 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.7.2'
+version          '0.7.3'
 
 # baseserver
-depends 'baseserver', '~> 0.7.10'
+depends 'baseserver', '~> 0.7.11'
 
 # webserver
 depends 'apache2', '~> 3.0.1'
 depends 'php', '~> 1.5.0'
+depends 'logrotate', '~> 1.8.0'
 
 # database
 depends 'database', '~> 2.3.1'
