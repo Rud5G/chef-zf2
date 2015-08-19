ac94dcf84688f3a4276865995b004e695e296477
Rudger Gravestein <rudgergravestein@gmail.com>
2015-08-19 11:40:28 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.9.1'
+version          '0.9.2'
 
 # baseserver
 depends 'baseserver', '~> 0.8.5'
