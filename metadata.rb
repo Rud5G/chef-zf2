5002a9f6164b69935849d8a51f9cac4d190520c1
Rudger Gravestein <rudgergravestein@gmail.com>
2015-09-30 16:24:58 +0200

    Bump version: 0.9.6
    Add db_migration to deploy

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-version          '0.9.5'
+version          '0.9.6'
 
 # baseserver
 depends 'baseserver', '~> 0.8.5'
