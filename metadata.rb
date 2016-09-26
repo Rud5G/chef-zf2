11829d64a9c7de42c4aa18c14e5c0497001ece35
Rudger Gravestein <rudgergravestein@gmail.com>
2016-09-26 13:50:42 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
 description 'Installs/Configures ZF2'
 long_description 'Installs/Configures ZF2'
-# issues_url 'https://github.com/Rud5G/chef-zf2/issues' if respond_to?(:issues_url)
-# source_url 'https://github.com/Rud5G/chef-zf2' if respond_to?(:source_url)
+issues_url 'https://github.com/Rud5G/chef-zf2/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/Rud5G/chef-zf2' if respond_to?(:source_url)
 version '0.11.6'
 
 # support
